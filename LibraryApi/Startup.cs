63881555ca5684c2007020e5ce274bb909cdf52a
Jeffry Gonzalez <jeff@hypertheory.com>
2020-04-01 15:35:10 -0400

    end of day wednesday

--- LibraryApi/Startup.cs
+++ LibraryApi/Startup.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Reflection;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 using LibraryApi.Controllers;
 using LibraryApi.Domain;
@@ -33,7 +34,12 @@ namespace LibraryApi
         {
             services.AddTransient<ILookupOnCallDevelopers, MicrosoftTeamsOnCallDeveloperLookup>();
             services.AddTransient<IGenerateEmployeeIds, EmployeeIdGenerator>();
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                {
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+                    options.JsonSerializerOptions.IgnoreNullValues = true;
+                });
             services.AddDbContext<LibraryDataContext>(options =>
                 options.UseSqlServer(Configuration.GetConnectionString("LibraryDatabase"))
 
